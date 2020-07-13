b5f3fab36d389994729e6c09c893ebc10c86fa4d
Juan-Ceballos <juanceballos@pursuit.org>
2020-07-12 21:28:25 -0400

    created board html, css. title off center w/ board.

--- ticTacToe.js
+++ ticTacToe.js
@@ -3,27 +3,9 @@ let header = document.createElement("header");
 let webTitle = document.createElement("h1");
 webTitle.innerHTML = "Tic-Tac-Toe";
 webTitle.style.textAlign = "center";
+webTitle.style.margin = "auto";
 
 document.body.appendChild(header);
 header.appendChild(webTitle);
 
 document.body.style.backgroundColor = "#12cad6";
-
-// let boardArea = document.createElement("div");
-
-// let div0 = document.createElement("div");
-// let div1 = document.createElement("div");
-// let div2 = document.createElement("div");
-// let div3 = document.createElement("div");
-// let div4 = document.createElement("div");
-// let div5 = document.createElement("div");
-// let div6 = document.createElement("div");
-// let div7 = document.createElement("div");
-// let div8 = document.createElement("div");
-// let div9 = document.createElement("div");
-
-// btn0.style.backgroundColor = "green";
-
-// boardArea.appendChild(btn0);
-// document.body.appendChild(boardArea);
-// document.createElement("")
