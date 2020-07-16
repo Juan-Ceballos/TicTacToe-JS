f881dd15ab33cdad4df1ba5367857cc484e6ecab
Juan-Ceballos <juanceballos@pursuit.org>
2020-07-16 12:41:11 -0400

    moved visual elements to html file to apply js logic onload

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,11 +1,3 @@
-let header = document.createElement("header");
-
-let webTitle = document.createElement("h1");
-webTitle.innerHTML = "Tic-Tac-Toe";
-webTitle.style.textAlign = "center";
-webTitle.style.margin = "auto";
-
-document.body.appendChild(header);
-header.appendChild(webTitle);
-
 document.body.style.backgroundColor = "#12cad6";
+
+let board = document.getElementById("brd");
