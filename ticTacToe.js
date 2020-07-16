038ae670aea2ee542fd28f627aedc441f3e965a3
Juan-Ceballos <juanceballos@pursuit.org>
2020-07-16 17:26:57 -0400

    onClick functionality started

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,3 +1,15 @@
 document.body.style.backgroundColor = "#12cad6";
 
 let board = document.getElementById("brd");
+let boardRow = board.children[0];
+let boardCell = boardRow.children[0];
+
+// set up on click on click event
+
+boardCell.style.backgroundColor = "red";
+// board.onclick = function () {
+//   boardCell.style.backgroundColor = "yellow";
+// };
+boardRow.onclick = function () {
+  boardCell.style.backgroundColor = "yellow";
+};
