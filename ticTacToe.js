bd6f77da42d2c0c02c3c221d4b10a784db6141d8
Juan-Ceballos <juanceballos@pursuit.org>
2020-07-17 16:33:44 -0400

    cleaned up using div, and flex rather than inline blocks, used border to seperate board cells

--- ticTacToe.js
+++ ticTacToe.js
@@ -4,12 +4,20 @@ let board = document.getElementById("brd");
 let boardRow = board.children[0];
 let boardCell = boardRow.children[0];
 
+// let x = document.createElement("div");
+// x.style.verticalAlign = "center";
+// x.style.textAlign = "center";
+// x.innerHTML = "X";
 // set up on click on click event
 
 boardCell.style.backgroundColor = "red";
 // board.onclick = function () {
 //   boardCell.style.backgroundColor = "yellow";
 // };
-boardRow.onclick = function () {
-  boardCell.style.backgroundColor = "yellow";
+// boardRow.onclick = function () {
+//   boardCell.innerHTML = "X";
+// };
+
+boardCell.onclick = function () {
+  boardCell.innerHTML = "X";
 };
