44891d316ecc252f6926392ec38d1309f0730a95
Juan-Ceballos <juanceballos@pursuit.org>
2020-07-18 18:13:14 -0400

    refactored for single arr rather than matrix, eventListener then save clicked cell and index on click event

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,41 +1,42 @@
 document.body.style.backgroundColor = "#12cad6";
 
+// setup game
+// board, player turn, win conditions, is game over?,
 let board = document.getElementById("brd");
-let boardRow = board.children[0];
-let boardCell = boardRow.children[0];
 
 let boardArr = [
   [0, 0, 0],
   [0, 0, 0],
   [0, 0, 0],
 ];
-// board[0] = [d1, d2, d3]
-console.log(board.children.length);
-console.log(board.children[0].length); //undefined
-console.log(boardRow.length); // also prints undefined
 
-for (let i = 0; i <= board.children.length - 1; i++) {
-  let br = board.children[i].children;
-  for (let j = 0; j <= br.length - 1; j++) {
-    console.log(j);
-  }
-  console.log(i);
+let playerTurn = 0;
+function handleCellClick(clickedCellEvent) {
+  console.log("hellos");
+  const clickedCell = clickedCellEvent.target;
+
+  const clickedCellIndex = parseInt(
+    clickedCell.getAttribute("data-cell-index")
+  );
+
+  console.log(clickedCell);
+  console.log(clickedCellIndex);
 }
 
-boardCell.onclick = function () {
-  boardCell.innerHTML = "X";
-  boardCell.style.textAlign = "center";
-  boardCell.style.lineHeight = "150px";
-};
+// addEventListener, .addTarget(swift)
+// querySA returns node list, node list collection of elements
+// node list is not an array but can iterate like array using forEach
+document
+  .querySelectorAll(".cell")
+  .forEach((cell) => cell.addEventListener("click", handleCellClick));
+//---------------------------------------------------------------
+//boardCell.style.lineHeight = "150px";
 
-board.onclick = function () {
-  boardCell.innerHTML = "O";
-  boardCell.style.textAlign = "center";
-  boardCell.style.lineHeight = "150px";
-};
-
-// 3x3 board
-// register click on every cell corresponds appropriately
-function boardTurn() {
-  // cell id,
-}
+// sample for loop for going through divs
+// for (let i = 0; i <= board.children.length - 1; i++) {
+//   let br = board.children[i].children;
+//   for (let j = 0; j <= br.length - 1; j++) {
+//     console.log(j);
+//   }
+//   console.log(i);
+// }
