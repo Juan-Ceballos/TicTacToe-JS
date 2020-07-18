faf9dac00c760067ec5c80a543b2ebce79e4ad80
Juan-Ceballos <juanceballos@pursuit.org>
2020-07-18 14:21:14 -0400

    cleaned up comment code, testing nested array print for html elements children

--- ticTacToe.js
+++ ticTacToe.js
@@ -4,20 +4,38 @@ let board = document.getElementById("brd");
 let boardRow = board.children[0];
 let boardCell = boardRow.children[0];
 
-// let x = document.createElement("div");
-// x.style.verticalAlign = "center";
-// x.style.textAlign = "center";
-// x.innerHTML = "X";
-// set up on click on click event
+let boardArr = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+];
+// board[0] = [d1, d2, d3]
+console.log(board.children.length);
+console.log(board.children[0].length); //undefined
+console.log(boardRow.length); // also prints undefined
 
-boardCell.style.backgroundColor = "red";
-// board.onclick = function () {
-//   boardCell.style.backgroundColor = "yellow";
-// };
-// boardRow.onclick = function () {
-//   boardCell.innerHTML = "X";
-// };
+for (let i = 0; i <= board.children.length - 1; i++) {
+  let br = board.children[i].children;
+  for (let j = 0; j <= br.length - 1; j++) {
+    console.log(j);
+  }
+  console.log(i);
+}
 
 boardCell.onclick = function () {
   boardCell.innerHTML = "X";
+  boardCell.style.textAlign = "center";
+  boardCell.style.lineHeight = "150px";
 };
+
+board.onclick = function () {
+  boardCell.innerHTML = "O";
+  boardCell.style.textAlign = "center";
+  boardCell.style.lineHeight = "150px";
+};
+
+// 3x3 board
+// register click on every cell corresponds appropriately
+function boardTurn() {
+  // cell id,
+}
